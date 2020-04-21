46cef7f7e2489d83ab0fd4387b44d1f5dd479b64
Levi Cai <cail@mit.edu>
2020-04-21 18:44:28 -0400

    Added a mini simulation

--- quad2d.m
+++ quad2d.m
@@ -11,11 +11,14 @@ clear all
 m = 0.486;
 r = 0.25;
 iz = 0.00383;
-g = 2;
-dt = 0.1;
+g = 9.81;
+
+dt = 0.03;
+plot_limit = 10;
+final_eps = 0.1;
 
 % initial conditions
-x0 = [0 10 0 0 0 0];
+x0 = [0 0 0 0 0 0];
 u0 = m*g*0.5*[1 1];
 
 % LQR
@@ -40,10 +43,64 @@ B_sym = jacobian(f,[u1 u2]);
 A = eval(subs(A_sym,[x1 x2 x3 x4 x5 x6 u1 u2],[x0 u0]));
 B = eval(subs(B_sym,[x1 x2 x3 x4 x5 x6 u1 u2],[x0 u0]));
 
-%% LQR
+%% Discrete-LQR
 K = lqrd(A,B,Q,R,dt);
 
-%% Simulation
+%% Slow Simulation
+figure;
+hold on;
+xlim([-plot_limit plot_limit]);
+ylim([-plot_limit plot_limit]);
+
+ts = 0:dt:5;
+x = rand(6,1);
+x(1:2) = x(1:2) * plot_limit;
+xs = [x];
+qx = x(1);
+qy = x(2);
+
+plot(x(1,1),x(2,1),'gx');
+
+p = plot(qx,qy);
+p.XDataSource = 'qx';
+p.YDataSource = 'qy';
+
+qpx = [x(1)-cos(x(3)) x(1)+cos(x(3))];
+qpy = [x(2)-sin(x(3)) x(2)+sin(x(3))];
+qp = plot(qpy, qpx, 'r-');
+qp.XDataSource = 'qpx';
+qp.YDataSource = 'qpy';
+
+for t = ts
+    % Update dynamics
+    u = -K*x;
+    xd = A*x + B*u;
+    x = x + xd * dt;
+    xs = [xs x];
+    
+    % Update simulation data
+    qx = xs(1,:);
+    qy = xs(2,:);
+    
+    qpx = [x(1)-cos(x(3)) x(1)+cos(x(3))];
+    qpy = [x(2)-sin(x(3)) x(2)+sin(x(3))];
+    
+    % Update the simulation
+    refreshdata
+    drawnow
+    pause(dt);
+    
+    if norm(x) < final_eps
+        break;
+    end
+end
+
+plot(xs(1,end),xs(2,end),'ro');
+disp('done')
+hold off;
+
+%% Fast Simulation
+%{
 ts = 0:dt:15;
 x = rand(6,1);
 xs = [x];
@@ -62,3 +119,4 @@ plot(xs(1,1),xs(2,1),'gx');
 plot(xs(1,end),xs(2,end),'ro');
 plot(xs(1,:),xs(2,:))
 hold off;
+%}
