4fee041cfb23b273b6fe84aba58ae80fdd45bd09
san-soucie <44901782+san-soucie@users.noreply.github.com>
2020-04-29 14:25:45 -0400

    Linear MPC is tested and working. Simple mass-spring-damper model demo

--- src/linearization.jl
+++ src/linearization.jl
@@ -21,30 +21,30 @@ function linearize(f, x_G, u_G)
     return A, B
 end
 
-make_linear_system(f, x_G, u_G) = LinearSystem(linearize(f, x_G, u_G)...)
+# make_linear_system(f, x_G, u_G) = LinearSystem(linearize(f, x_G, u_G)...)
 
 to_col(x::AbstractVector{T}) where {T} = reshape(x, (length(x), 1))
 to_row(x::AbstractVector{T}) where {T} = reshape(x, (1, length(x)))
 
-
-struct LinearSystem{T<:Real,M<:AbstractMatrix{T}}
-    A::M
-    B::M
-    function LinearSystem{T,M}(
-        A::M,
-        B::M,
-    ) where {T<:Real} where {M<:AbstractMatrix{T}}
-        size(A)[1] == size(A)[2] || throw(ArgumentError("A must be square"))
-        size(A)[1] == size(B)[1] ||
-        throw(ArgumentError("A and B must have the same number of rows"))
-        new{T,M}(A, B)
-    end
-end
-
-LinearSystem(A <: AbstractMatrix{T}, B <: AbstractVector{T}) where {T<:Real} =
-    LinearSystem(A, to_col(B))
-
-(m::LinearSystem{T,M})(x::M, u::M) = m.A * x + m.B * u
-(m::LinearSystem{T,M})(x::V{<:T}, u::V{<:T}) where {V<:AbstractVector} =
-    m.A * x + m.B * u
-(m::LinearSystem{T,M})(x, u, t) = m(x, u)
+#
+# struct LinearSystem{T,M<:AbstractMatrix{T}}
+#     A::M
+#     B::M
+#     function LinearSystem{T,M}(
+#         A::M,
+#         B::M,
+#     ) where {T<:Real} where {M<:AbstractMatrix{T}}
+#         size(A)[1] == size(A)[2] || throw(ArgumentError("A must be square"))
+#         size(A)[1] == size(B)[1] ||
+#         throw(ArgumentError("A and B must have the same number of rows"))
+#         new{T,M}(A, B)
+#     end
+# end
+#
+# LinearSystem(A <: AbstractMatrix{T}, B <: AbstractVector{T}) where {T<:Real} =
+#     LinearSystem(A, to_col(B))
+#
+# (m::LinearSystem{T,M})(x::M, u::M) = m.A * x + m.B * u
+# (m::LinearSystem{T,M})(x::V{<:T}, u::V{<:T}) where {V<:AbstractVector} =
+#     m.A * x + m.B * u
+# (m::LinearSystem{T,M})(x, u, t) = m(x, u)
--- src/mpc.jl
+++ src/mpc.jl
@@ -1,5 +1,6 @@
 using JuMP, MosekTools
 using PolyJuMP, SumOfSquares
+using Plots
 include("linearization.jl")
 
 # f: one-time-step system dynamics
@@ -20,17 +21,17 @@ function mpc(f, ℓ, x̂ᵢ, xᵢʳᵉᶠ, uᵢʳᵉᶠ, N)
     rᵢ = A * xᵢʳᵉᶠ[:, 1:N] + B * uᵢʳᵉᶠ - xᵢʳᵉᶠ[:, 2:N+1] # Dynamics error in desired trajectory
     cost = ℓ(Δxᵢ[:, 1:N], Δuᵢ)
     for n in 1:dim_x
-        @constraint m Δxᵢ[n, 1] = x̂ᵢ[n] - xᵢʳᵉᶠ[n, 1] # Enforces start point
+        @constraint(m, Δxᵢ[n, 1] == x̂ᵢ[n] - xᵢʳᵉᶠ[n, 1]) # Enforces start point
     end
     for k in 1:N
         Aᵢₖ, Bᵢₖ = linearize(f, xᵢʳᵉᶠ[:, k], uᵢʳᵉᶠ[:, k])
         rᵢ = Aᵢₖ * xᵢʳᵉᶠ[:, k] + Bᵢₖ * uᵢʳᵉᶠ[:, k] - xᵢʳᵉᶠ[:, k+1]
         Δxᵢₖ₊₁ = Aᵢₖ * Δxᵢ[:, k] + Bᵢₖ * Δuᵢ[:, k] - rᵢ # Dynamics
         for n in 1:dim_x
-            @constraint m Δxᵢ[n, 2:N+1] = Δxᵢₖ₊₁[n] # Enforces dynamics
+            @constraint(m, Δxᵢ[n, 2:N+1] == Δxᵢₖ₊₁[n]) # Enforces dynamics
         end
     end
-    @objective model Min cost
+    @objective(model, Min, cost)
     optimize!(model)
 end
 
@@ -43,20 +44,60 @@ function linear_mpc(A, B, Q, R, x̂ᵢ, xᵢʳᵉᶠ, uᵢʳᵉᶠ, N)
 
 
     for n in 1:dim_x
-        @constraint m Δxᵢ[n, 1] = x̂ᵢ[n] - xᵢʳᵉᶠ[n, 1]
+        @constraint(m, Δxᵢ[n, 1] == x̂ᵢ[n] - xᵢʳᵉᶠ[n, 1])
     end
 
     rᵢ = A * xᵢʳᵉᶠ[:, 1:N] + B * uᵢʳᵉᶠ - xᵢʳᵉᶠ[:, 2:N+1]
     Δxᵢₖ₊₁ = A * Δxᵢ[:, 1:N] + B * Δuᵢ - rᵢ
     for k in 1:N
         for n in 1:dim_x
-            @constraint m Δxᵢ[n, 2:N+1] = Δxᵢₖ₊₁[n, k]
+            @constraint(m, Δxᵢ[n, k+1] == Δxᵢₖ₊₁[n, k])
         end
     end
     cost = sum(
         Δxᵢ[:, k]' * Q * Δxᵢ[:, k] + Δuᵢ[:, k]' * R * Δuᵢ[:, k] for k in 1:N
     )
-    @objective(model, Min, cost)
-    optimize!(model)
-    return model
+    @objective(m, Min, cost)
+    optimize!(m)
+    return Δxᵢ, Δuᵢ, m
 end
+
+function main()
+    num_iters = 10 # Number of MPC optimizations to run
+    reject_ratio = 0.8 # Fraction of trajectory to throw out
+    N = 15 # number of iterations per MPC optimization; dt = Δt / N
+    Δt = 0.75 # Timespan of single MPC optimization
+
+    k = 16.0
+    b = 0.8
+    m = 1.0
+    A = [0.0 1.0; -k / m b / m] * Δt + [1.0 0.0; 0.0 1.0]
+    B = [0.0; 1.0] * Δt
+    B = reshape(B, (length(B), 1))
+    Q = [0.001 0.0; 0.0 0.001]
+    R = reshape([100.0], (1, 1))
+    x₀ = [0.0; 0.0]
+
+
+    xG = [4.0 0.0]
+    uG = [(k / m) * xG[1]]
+    xᵢʳᵉᶠ = repeat(xG, N + 1)' |> collect
+    uᵢʳᵉᶠ = repeat(uG, N)' |> collect
+    us = reshape(typeof(uG)[], (1, 0))
+    xs = reshape(typeof(xG)[], (2, 0))
+
+    reject = Int(reject_ratio * N)
+    for i in 1:num_iters
+        x̂ᵢ = (i == 1) ? x₀ : xs[:, end]
+        x, u, m = linear_mpc(A, B, Q, R, x̂ᵢ, xᵢʳᵉᶠ, uᵢʳᵉᶠ, N)
+        xs = hcat(xs, value.(x)[:, 1:end-reject] + xᵢʳᵉᶠ[:, 1:end-reject])
+        us = hcat(us, value.(u) + uᵢʳᵉᶠ)
+    end
+
+    t = collect(1:size(xs)[2]) * Δt / N
+    display(plot(t, xs[1, :]))
+
+    return xs, us
+end
+
+main()
