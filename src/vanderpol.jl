56b350d308438bc0b7e3296b5705e3492fbf3283
Levi Cai <cail@mit.edu>
2020-04-30 00:24:10 -0400

    Initial attempt at vanderpol ROA analysis (does NOT work)

--- src/vanderpol.jl
+++ src/vanderpol.jl
@@ -14,22 +14,37 @@ function vanderpol(state)
     ]
 end
 
-fun[1:ction linearize(f, x_G)
+function linearize(f, x_G)
     A = jacobian(x -> f(x), x_G)
     return A
+end
+
+function optimize_ρ(ρ, J★, J̇̂★, n, ϵ)
+    model =
+        SOSModel(optimizer_with_attributes(Mosek.Optimizer, "QUIET" => true))
+    @polyvar x̅[1:n]
+    #@variable model ρ
+    X = monomials(x̅, 0:2)
+    @variable model h SOSPoly(X)
+    @constraint model h in SOSCone()
+    @constraint model J̇̂★(x̅) + h * (ρ - J★(x̅)) <= -ϵ * (x̅' * x̅)
+    #@objective(model, Max, ρ)
+    optimize!(model)
+    print("done")
+    print(termination_status(model))
+    return model
 end
 
 function main()
     # Take jacobian
     xG = [0;0]
     A = linearize(vanderpol, xG)
-    Q = I
+    Q = [1 0; 0 1]
     P = lyap(A,Q)
 
-    # SOS?
-    #m = Model(optimizer_with_attributes(Mosek.Optimizer, "QUIET" => true))
-
-
+    ρ = 1
+    m = optimize_ρ(ρ, x -> x'*P*x, x -> -2*x'*P*vanderpol(x), 2, 1e-3)
+    m
 end
 
 main()
